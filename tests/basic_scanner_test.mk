1f856a6d39fd486a0dd8a5a66e0e2785148212c7
Michael Builov <mbuilov@gmail.com>
2017-02-17 13:39:27 +0300

    support tests for NO_STATIC=1 builds

--- tests/basic_scanner_test.mk
+++ tests/basic_scanner_test.mk
@@ -1,9 +1,23 @@
 include $(dir $(lastword $(MAKEFILE_LIST)))../top.mk
 include $(MTOP)/c.mk
 include $(MTOP)/ctest.mk
+
 EXE     := basic_scanner_test
-LIBS    := gtparser
 SRC     := basic_scanner_test.c
 INCLUDE := ..
-$(DO_TEST_EXE)
+RPATH   := $(LIB_DIR)
+
+ifndef NO_STATIC
+LIBS    := gtparser
+else ifndef NO_SHARED
+DLLS    := gtparser
+DEFINES := GTPARSER_EXPORTS=$(DLL_IMPORTS_DEFINE)
+else
+$(error cannot link gtparser library for tests)
+endif
+
+SHLIBS := $(DLLS:=.$(call ver_major,$(PRODUCT_VER)))
+$(call TEST_NEED_SIMLINKS,$(SHLIBS))
+$(call DO_TEST_EXE,$(SHLIBS))
+
 $(DEFINE_TARGETS)
--- tests/cstring_parser_test.mk
+++ tests/cstring_parser_test.mk
@@ -1,9 +1,22 @@
 include $(dir $(lastword $(MAKEFILE_LIST)))../top.mk
 include $(MTOP)/c.mk
 include $(MTOP)/ctest.mk
+
 EXE     := cstring_parser_test
-LIBS    := gtparser
 SRC     := cstring_parser_test.c
 INCLUDE := ..
-$(DO_TEST_EXE)
+RPATH   := $(LIB_DIR)
+
+ifndef NO_STATIC
+LIBS    := gtparser
+else ifndef NO_SHARED
+DLLS    := gtparser
+else
+$(error cannot link gtparser library for tests)
+endif
+
+SHLIBS := $(DLLS:=.$(call ver_major,$(PRODUCT_VER)))
+$(call TEST_NEED_SIMLINKS,$(SHLIBS))
+$(call DO_TEST_EXE,$(SHLIBS))
+
 $(DEFINE_TARGETS)
