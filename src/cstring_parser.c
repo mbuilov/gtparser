507d634499adebadaed1421db98a9e5fb8e7e8e7
Michael Builov <mbuilov@gmail.com>
2018-05-21 18:31:08 +0300

    avoid casting unsigned integer to signed one (char) of different size

--- src/cstring_parser.c
+++ src/cstring_parser.c
@@ -100,7 +100,7 @@ GTPARSER_EXPORTS void gt_copy_cstring(char dst[]/*out*/, const char *begin, cons
 								}
 							}
 						}
-						c = (char)n; /* < 256 */
+						c = (char)(unsigned char)n; /* < 256 */
 					}
 					break;
 				case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7': {
@@ -127,7 +127,7 @@ GTPARSER_EXPORTS void gt_copy_cstring(char dst[]/*out*/, const char *begin, cons
 							}
 						}
 					}
-					c = (char)n; /* < 256 */
+					c = (char)(unsigned char)n; /* < 256 */
 					break;
 				}
 				default:
