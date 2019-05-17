3e55d05c9e097d57c6abfa22a198b3882028ca42
Michael Builov <mbuilov@gmail.com>
2019-05-17 16:11:36 +0300

    add GT_UINT_ALL_GE(), replace GT_UINT_CONTAINS_ZERO() -> GT_UINT_ALL_NOZERO()

--- gtparser/int_char.h
+++ gtparser/int_char.h
@@ -47,48 +47,71 @@
 	0*sizeof(int[1-2*(sizeof(type) < sizeof(b))]))
 #endif
 
-/* convert character to unisgned integer,
-  because char may be signed, need to mask-out sign-extended bits */
-#define GT_CHAR_TO_UINT(c) ((unsigned)(c) & /*mask-out sign-extended bits*/(  \
-	/*byte has 8 bits*/0*sizeof(int[1-2*(255 != (unsigned char)-1)]) +        \
-	/*char type is small*/0*sizeof(int[1-2*(sizeof(c) > sizeof(unsigned))]) + \
-	/*construct mask*/(((1u << (8*sizeof(c) - 1)) - 1u) | (1u << (8*sizeof(c) - 1)))))
+/* top-most bit for a given character type:
+  char    -> 0x80
+  wchar_t -> 0x8000 (windows) or 0x80000000 (unix) */
+#define GT_CHAR_TYPE_TOP_BIT(char_type) (1u << (sizeof(char_type)*8 - 1))
 
-/* check that chars read into an integer are latin letters (there are only 26 letters),
+/* convert character to unsigned integer,
+  because char may be signed, need to mask-out sign-extended bits */
+#define GT_CHAR_TO_UINT(char_type, c) ((unsigned)(c) & /*mask-out sign-extended bits*/(           \
+	/*byte has 8 bits*/0*sizeof(int[1-2*(255 != (unsigned char)-1)]) +                            \
+	/*char type is small*/0*sizeof(int[1-2*(sizeof(char_type) > sizeof(unsigned))]) +             \
+	/*construct mask*/((GT_CHAR_TYPE_TOP_BIT(char_type) - 1u) | GT_CHAR_TYPE_TOP_BIT(char_type))))
+
+#define GT_IS_LATIN_UINT_IN_RANGE_(char_type, uint_type, l, u, c, r) do {                                  \
+	/* assume: (u - l) < 32,                                                                               \
+	  .............bbbbb.......                                                                            \
+	   ^0   ^u-31  ^l  ^u                                                                                  \
+	 1) check that all chars of int are >= l: (c - l) should not overflow                                  \
+	 2) check that all chars of int are < u:  (c - (u-31)) should be < 32 */                               \
+	const uint_type gt_ll_ = GT_ONE_ONE_CONST(char_type, uint_type)*GT_CHAR_TO_UINT(char_type, l);         \
+	const uint_type gt_uu_ = GT_ONE_ONE_CONST(char_type, uint_type)*(GT_CHAR_TO_UINT(char_type, u) - 31u); \
+	const uint_type gt_zz_ = ~(GT_ONE_ONE_CONST(char_type, uint_type)*0x1F);                               \
+	const uint_type gt_cc_ = (c);                                                                          \
+	const uint_type gt_x_ = GT_UINT_DIFF(uint_type, gt_cc_, gt_ll_);                                       \
+	const uint_type gt_y_ = GT_UINT_DIFF(uint_type, gt_cc_, gt_uu_);                                       \
+	r = !((gt_x_ | gt_y_) & gt_zz_);                                                                       \
+} while (0)
+
+#define GT_UINT_ALL_GE_(char_type, uint_type, b, c, r) do {                                        \
+	const uint_type gt_cc_ = (c) + 0*sizeof(int[1-2*(sizeof(uint_type) != sizeof(c))]);            \
+	const uint_type gt_bb_ = GT_ONE_ONE_CONST(char_type, uint_type)*GT_CHAR_TO_UINT(char_type, b); \
+	r = !((gt_cc_ - gt_bb_) & ~gt_cc_ &                                                            \
+		GT_ONE_ONE_CONST(char_type, uint_type)*GT_CHAR_TYPE_TOP_BIT(char_type));                   \
+} while (0)
+
+/* check that all chars read into an integer are latin letters (there are only 26 letters),
  e.g.:
   int r;
   unsigned c;
   memcpy(&c, L"string", sizeof(c));
   GT_IS_LATIN_UINT_IN_RANGE(wchar_t, unsigned, L'a', L'z', c, r);
 */
-#define GT_IS_LATIN_UINT_IN_RANGE(char_type, uint_type, l, u, c, r) do {                        \
-	/* assume: (u - l) < 32,                                                                    \
-	  .............bbbbb.......                                                                 \
-	   ^0   ^u-31  ^l  ^u                                                                       \
-	 1) check that all chars of int are >= l: (c - l) should not overflow                       \
-	 2) check that all chars of int are < u:  (c - (u-31)) should be < 32 */                    \
-	const uint_type gt_ll_ = GT_ONE_ONE_CONST(char_type, uint_type)*GT_CHAR_TO_UINT(l);         \
-	const uint_type gt_uu_ = GT_ONE_ONE_CONST(char_type, uint_type)*(GT_CHAR_TO_UINT(u) - 31u); \
-	const uint_type gt_zz_ = ~(GT_ONE_ONE_CONST(char_type, uint_type)*0x1F);                    \
-	const uint_type gt_cc_ = (c);                                                               \
-	const uint_type gt_x_ = GT_UINT_DIFF(uint_type, gt_cc_, gt_ll_);                            \
-	const uint_type gt_y_ = GT_UINT_DIFF(uint_type, gt_cc_, gt_uu_);                            \
-	/*check that (u - l) < 32*/(void)sizeof(int[1-2*((u) - (l) >= 32)]);                        \
-	r = !((gt_x_ | gt_y_) & gt_zz_);                                                            \
-} while (0)
+#define GT_IS_LATIN_UINT_IN_RANGE(char_type, uint_type, l, u, c, r)                         \
+	GT_IS_LATIN_UINT_IN_RANGE_(char_type, uint_type,                                        \
+		(/*check that (u - l) < 32*/(void)sizeof(int[1-2*((u) - (l) >= 32)]),(l)), u, c, r)
+
+/* check that all chars read into an integer are greater or equal to some (small) char
+ e.g.:
+  int r;
+  unsigned c;
+  memcpy(&c, L"string", sizeof(c));
+  GT_UINT_ALL_GE(wchar_t, unsigned, L' ', c, r);
+*/
+#define GT_UINT_ALL_GE(char_type, uint_type, b, c, r)                                      \
+	GT_UINT_ALL_GE_(char_type, uint_type,                                                  \
+		(/*check that b is small enough*/(void)sizeof(int[1-2*(                            \
+			GT_CHAR_TYPE_TOP_BIT(char_type) & GT_CHAR_TO_UINT(char_type, b))]),(b)), c, r)
 
 /* check if there is a zero char among chars read into an integer,
  e.g.:
   int r;
   unsigned c;
   memcpy(&c, L"string", sizeof(c));
-  GT_UINT_CONTAINS_ZERO(wchar_t, unsigned, c, r);
+  GT_UINT_ALL_NOZERO(wchar_t, unsigned, c, r);
 */
-#define GT_UINT_CONTAINS_ZERO(char_type, uint_type, c, r) do {  \
-	const uint_type gt_cc_ = (c) +                              \
-		0*sizeof(int[1-2*(sizeof(uint_type) != sizeof(c))]);    \
-	r = !!((gt_cc_ - GT_ONE_ONE_CONST(char_type, uint_type)) &  \
-		GT_ONE_ONE_CONST(char_type, uint_type)*0x80 & ~gt_cc_); \
-} while (0)
+#define GT_UINT_ALL_NOZERO(char_type, uint_type, c, r) \
+	GT_UINT_ALL_GE(char_type, uint_type, 1, c, r)
 
 #endif /* INT_CHAR_H_INCLUDED */
